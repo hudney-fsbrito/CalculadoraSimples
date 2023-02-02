125c25c16a1f4ab5ad0ba8412862f2b24e3172ec
hudney-fsbrito <hudney.rse@gmail.com>
2023-02-01 21:12:38 -0300

    Primeiro commit: layout do projeto.

---
+++ script.js
@@ -0,0 +1,11 @@
+
+let tela = document.getElementById("tela");
+let numeros = document.getElementsByClassName("num")
+
+
+
+    
+    function calc(e) {
+        tela.innerHTML = e.innerText
+        console.log(e);
+    }
