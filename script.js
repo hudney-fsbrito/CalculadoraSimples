1c1c5dd74262beece956054cb2b940dc9c241b47
hudney-fsbrito <hudney.rse@gmail.com>
2023-02-02 22:54:10 -0300

    feat: add os numeros concatenados na tela

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 
 let tela = document.getElementById("tela");
-let numeros = document.getElementsByClassName("num")
+let divNum = document.getElementsByClassName("num")
+let numero;
+let numero1;
+let numero2;
+let resultado;
 
 
+// console.log(divNum);
+// console.log(parseInt(divNum.innerText) + 2);
+// console.log(parseFloat(divNum[0].innerText) + 2);
+function calc(e) {
+    numero = parseFloat(e.innerText);
+    console.log(numero);
+    escrever()
+}
 
-    
-    function calc(e) {
-        tela.innerHTML = e.innerText
-        console.log(e);
-    }
+function escrever() {
+    tela.innerHTML += numero
+
+}
